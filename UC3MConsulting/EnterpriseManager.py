bce93d66df3b03dd64966574986b6bb8fea8a77c
Mohamed <100506445@alumnos.uc3m.es>
2026-02-09 19:46:08 +0100

    ValidateCiF function

--- UC3MConsulting/EnterpriseManager.py
+++ UC3MConsulting/EnterpriseManager.py
@@ -9,7 +9,42 @@ class EnterpriseManager:
     def ValidateCIF( self, CiF ):
         # PLEASE INCLUDE HERE THE CODE FOR VALIDATING THE GUID
         # RETURN TRUE IF THE GUID IS RIGHT, OR FALSE IN OTHER CASE
-        return True
+        if (
+                len(CiF) != 9
+                or CiF[0] not in ["A", "B", "E", "H", "K", "P", "Q", "S"]
+                or not CiF[1:8].isdigit()):
+            return False
+
+        letter = CiF[0]
+        number = CiF[1:8]
+        control = CiF[-1]
+        even = int(number[1]) + int(number[3]) + int(number[5])
+
+        odd = 0
+        for i in [0, 2, 4, 6]:
+            double = int(number[i]) * 2
+            if double >= 10:
+                double = (double // 10) + (double % 10)
+            odd += double
+
+        total = even + odd
+        if (total % 10) == 0:
+            base_digit = 0
+        else:
+            base_digit = 10 - (total % 10)
+
+        table = ["J", "A", "B", "C", "D", "E", "F", "G", "H", "I"]
+
+        if letter in ["A", "B", "E", "H"]:
+            if base_digit == int(control):
+                return True
+            else:
+                return False
+        elif letter in ["K", "P", "Q", "S"]:
+            if control == table[base_digit]:
+                return True
+            else:
+                return False
 
     def ReadproductcodefromJSON( self, fi ):
 
